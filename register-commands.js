859a94de3c5bdc3d7f3edaa73f1a4814e6a9c939
akinizer <joetsirius@gmail.com>
2023-10-24 23:58:23 +0300

    interactive help pagination + a list of commands

--- register-commands.js
+++ register-commands.js
@@ -2,10 +2,12 @@ import {REST,Routes} from "discord.js";
 import dotenv from "dotenv";
 dotenv.config();
 
+// bot token to connect to connect client and discord server; server id to run in channels; app id of bot
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 const GUILD_ID = process.env.GUILD_ID;
 const CLIENT_ID = process.env.CLIENT_ID;
 
+// command name after "/" and definition
 const commands = [
     {
         name: "hey",
@@ -14,11 +16,17 @@ const commands = [
     {
         name: "ping",
         description: "replies with  pong"
+    },
+    {
+        name: "help",
+        description: "opens commands help"
     }
 ];
 
+// declare rest api object with bot token
 const rest = new REST({version:"10"}).setToken(DISCORD_TOKEN);
 
+// Integrate slash commands to the console; the actions for commands are in index.js
 (async()=>{
     try{
         console.log("Registering slash commands...");
@@ -33,6 +41,7 @@ const rest = new REST({version:"10"}).setToken(DISCORD_TOKEN);
         console.log("Slash commands are registered successfully!");
     }
     catch(e){
+        // report error in log
         console.log(`${e}`);
     }
 })();
